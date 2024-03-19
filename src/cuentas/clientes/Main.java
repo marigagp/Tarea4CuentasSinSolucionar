ce71891b4964e2d8a5a1a4f2b3aa5b78a630d563
Usuario <Usuario@DESKTOP-8MVIA6J>
2024-03-20 00:00:43 +0100

    VINCULAR

---
+++ src/cuentas/clientes/Main.java
@@ -0,0 +1,31 @@
+package cuentas.clientes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        
+        System.out.println("El saldo actual es:"+ cuenta1.estado() );
+
+        operativa_cuenta(cuenta1, null);
+    }
+
+    private static void operativa_cuenta(Cuenta cuenta1, java.lang.Float cantidad) {
+        try {
+            cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
